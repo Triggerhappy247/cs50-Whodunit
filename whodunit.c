eaf45956e859ef3946b25b6bb469e2a414ba4156
Triggerhappy247 <qasim.bhatia@gmail.com>
2017-04-25 05:18:43 +0000

    added condition to change red pixels, produces black verdict

--- whodunit.c
+++ whodunit.c
@@ -75,6 +75,13 @@ int main(int argc, char *argv[])
 
             // read RGB triple from infile
             fread(&triple, sizeof(RGBTRIPLE), 1, inptr);
+            
+            if(triple.rgbtRed == 0xff)
+            {
+                triple.rgbtRed = 0x00;
+                triple.rgbtGreen = 0x00;
+                triple.rgbtBlue = 0x00;
+            }
 
             // write RGB triple to outfile
             fwrite(&triple, sizeof(RGBTRIPLE), 1, outptr);
