8869c6d6086cf5e76c715736ab2da585ff064b21
Triggerhappy247 <qasim.bhatia@gmail.com>
2017-04-25 05:21:10 +0000

    added condition to change red pixels to white instead of black, produces readable verdict

--- whodunit.c
+++ whodunit.c
@@ -76,11 +76,10 @@ int main(int argc, char *argv[])
             // read RGB triple from infile
             fread(&triple, sizeof(RGBTRIPLE), 1, inptr);
             
-            if(triple.rgbtRed == 0xff)
+            if(triple.rgbtBlue == 0x00 && triple.rgbtGreen == 0x00 && triple.rgbtRed == 0xff)
             {
-                triple.rgbtRed = 0x00;
-                triple.rgbtGreen = 0x00;
-                triple.rgbtBlue = 0x00;
+                triple.rgbtBlue = 0xff;
+                triple.rgbtGreen = 0xff;
             }
 
             // write RGB triple to outfile
